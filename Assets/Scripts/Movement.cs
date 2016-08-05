903f117035007c36dbdb214a066bdaebf1c0ea74
Hector C. Alzate <halzate93@gmail.com>
2016-08-04 20:01:09 -0500

    Initial Commit.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed;
+
+        private void Update()
+        {
+            float movement = Input.GetAxis("Vertical");
+			transform.Translate(Vector3.forward * speed * movement * Time.deltaTime);
+        }
+    }
+}
